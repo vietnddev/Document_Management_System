a0852aa1bbe5655ac47c820fc52321935182141d
vietnddev <nguyenducviet.vietnd@gmail.com>
2024-04-26 15:11:16 +0700

    Init the default export template path when starting the application

--- src/main/java/com/flowiee/dms/base/StartUp.java
+++ src/main/java/com/flowiee/dms/base/StartUp.java
@@ -6,6 +6,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
 
 @Configuration
@@ -22,10 +26,23 @@ public class StartUp {
             CommonUtils.START_APP_TIME = new Date();
             loadLanguageMessages("en");
             loadLanguageMessages("vi");
+            initReportConfig();
         };
     }
     
     private void loadLanguageMessages(String langCode) {
         languageService.reloadMessage(langCode);
+    }
+
+    public void initReportConfig() {
+        String templateExportTempStr = CommonUtils.templateExportExcelPath + "/temp";
+        Path templateExportTempPath = Paths.get(templateExportTempStr);
+        if (!Files.exists(templateExportTempPath)) {
+            try {
+                Files.createDirectory(templateExportTempPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
